4b746f6520186c9a03abfa340a87c40da9b18e7a
APSODE <2gunbo74@gmail.com>
2023-11-16 10:07:28 +0900

    image에 대한 전반적인 기능을 담당하는 Manager클래스

---
+++ core/manager/ImageManager.py
@@ -0,0 +1,108 @@
+import os.path
+import time
+from typing import Dict, List, Tuple
+from core.convertor.PixelConvertor import PixelConvertor
+from core.custom_decorator.RuntimeCounter import RuntimeCounter
+from core.type_annotation.CustomTypes import CHANNEL_TYPE_LITERAL, CHANNEL_LITERAL, ROTATE_TYPE, PIXEL_TYPE
+from core.wrapper.ImageWrapper import ImageWrapper
+from PIL.Image import Image
+from PIL.Image import new as new_image
+
+
+class ImageManager:
+    def __init__(self, image_dir: str, with_base_convertor: bool = False):
+        self._image_wrapper = ImageWrapper.create_object(image_dir, with_base_convertor)
+        self._loaded_image = self._image_wrapper.original_image
+        self._channel_amount_data: Dict[CHANNEL_TYPE_LITERAL, int] = {
+            "RGB": 3,
+            "YUV": 3,
+            "GRAY": 1
+        }
+
+    @staticmethod
+    def create_manager(image_dir: str) -> "ImageManager":
+        return ImageManager(image_dir)
+
+    @staticmethod
+    def save_image(file_name: str, image: Image):
+        from Testor import ROOT_PATH
+        image.save(os.path.join(ROOT_PATH, "converted_images", file_name))
+
+    @RuntimeCounter()
+    def image_split_by_channel(self, channel_type: CHANNEL_TYPE_LITERAL):
+        target_image: Image = getattr(self._image_wrapper, f"{channel_type.lower()}_image")
+        channel_images: List[Image]
+        channel_images = [new_image("L", target_image.size) for _ in range(self.get_channel_amount(channel_type))]
+        print(f"width : {target_image.width}\n"
+              f"height : {target_image.height}\n"
+              f"total : {target_image.width * target_image.height}"
+              )
+        # 반복문 실행횟수 최대 -> 3 * 1080 * 1920 = 6220800
+        for channel_idx in range(self.get_channel_amount(channel_type)):  # 1 or 3
+            for y_pos in range(target_image.height):  # 최대값 1080
+                for x_pos in range(target_image.width):  # 최대값 1920
+                    channel_images[channel_idx].putpixel(
+                        (x_pos, y_pos),
+                        self._get_split_pixel_data(
+                            target_pixel_data = target_image.getpixel((x_pos, y_pos)),
+                            channel_type = channel_type,
+                            channel_idx = channel_idx
+                        )
+                    )
+
+            self.save_image(
+                f"{channel_type}_channel_split_image_{channel_idx}.jpg",
+                channel_images[channel_idx]
+            )
+
+    @RuntimeCounter()
+    def rotate_image(self, rotate_type: ROTATE_TYPE):
+        rotated_image = new_image("RGB", self._loaded_image.size)
+        width, height = self._loaded_image.size
+        # rotate_range_param = self._create_rotate_range_param(rotate_type = rotate_type)
+
+        for y_pos in range(height):
+            for x_pos in range(width):
+                rotated_image.putpixel(
+                    self._create_rotate_pos(rotate_type = rotate_type, current_pos = (x_pos, y_pos)),
+                    self._loaded_image.getpixel((x_pos, y_pos))
+                )
+
+        self.save_image(f"{rotate_type}_rotated_image.jpg", rotated_image)
+
+    def get_channel_amount(self, channel_type: CHANNEL_TYPE_LITERAL) -> int:
+        return self._channel_amount_data.get(channel_type)
+
+    def _create_rotate_range_param(self, rotate_type: ROTATE_TYPE) -> Tuple[Tuple[int, int, int], Tuple[int, int, int]]:
+        width, height = self._loaded_image.size
+        rotate_operator = (-1, 1) if rotate_type == "vertical" else (1, -1)
+        y_pos_range_param: Tuple[int, int, int]
+        x_pos_range_param: Tuple[int, int, int]
+
+        if rotate_type == "vertical":
+            y_pos_range_param = (height - 1, -1, rotate_operator[0])
+            x_pos_range_param = (0, width, rotate_operator[1])
+        else:
+            y_pos_range_param = (0, height, rotate_operator[0])
+            x_pos_range_param = (width - 1, -1, rotate_operator[1])
+
+        return y_pos_range_param, x_pos_range_param
+
+    def _create_rotate_pos(self, rotate_type: ROTATE_TYPE, current_pos: Tuple[int, int]) -> Tuple[int, int]:
+        width, height = self._loaded_image.size
+
+        if rotate_type == "vertical":
+            return current_pos[0], ((height - 1) - current_pos[1])
+        else:
+            return ((width - 1) - current_pos[0]), current_pos[1]
+
+    @staticmethod
+    def _get_split_pixel_data(target_pixel_data: PIXEL_TYPE,
+                              channel_type: CHANNEL_TYPE_LITERAL,
+                              channel_idx: int) -> PIXEL_TYPE:
+
+        if channel_type in ["RGB", "YUV"]:
+            return target_pixel_data[channel_idx]
+
+        else:
+            return target_pixel_data
