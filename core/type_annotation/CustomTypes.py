83ed89ad2359baf64a522abc3fa9824939dee3b5
APSODE <2gunbo74@gmail.com>
2023-11-17 13:42:05 +0900

    NumPy ndarray지원을 위한 타입 어노테이션 수정

--- core/type_annotation/CustomTypes.py
+++ core/type_annotation/CustomTypes.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Union, Literal, Type
+from numpy import ndarray
 
-PIXEL_TYPE = Union[Tuple[int, int, int], int]
+PIXEL_TYPE = Union[Tuple[int, int, int], int, ndarray]
 CHANNEL_TYPE_LITERAL = Literal["RGB", "YUV", "GRAY"]
 CHANNEL_NAME_LITERAL = Literal["RGB", "YCbCr", "L"]
 CHANNEL_LITERAL = Literal["R", "G", "B", "Y", "U", "V", "L"]
