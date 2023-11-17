77d1df80f7a9d2253d120edf05b1a2581dbb4565
APSODE <2gunbo74@gmail.com>
2023-11-17 13:39:32 +0900

    실행시간 출력 방식 수정 및 출력 정보 추가

--- core/custom_decorator/RuntimeCounter.py
+++ core/custom_decorator/RuntimeCounter.py
@@ -1,18 +1,20 @@
 import time
-from typing import Callable
+from typing import Callable, Optional
 
 
 class RuntimeCounter:
-    def __init__(self):
-        pass
+    def __init__(self, description: Optional[str] = None):
+        self._description = description
 
     def __call__(self, method: Callable):
         def wrapper(instance, *args, **kwargs):
+            if self._description is not None:
+                print(self._description)
+
             st = time.perf_counter()
             method_result = method(instance, *args, **kwargs)
             et = time.perf_counter()
-
-            print(f"runtime : {round((et - st), 2)}s")
+            print(f"runtime : {round((et - st), 2)}s\n\n")
 
             return method_result
 
