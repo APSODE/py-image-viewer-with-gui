20a9b8443fce74216c916682f839a2595d1ea13b
APSODE <2gunbo74@gmail.com>
2023-11-22 13:54:02 +0900

    ComponentManager 클래스 업데이트

--- gui/ComponentManager.py
+++ gui/ComponentManager.py
@@ -27,12 +27,26 @@ class ComponentManager:
         return type_str.split("'")[1].split(".")[1 if "ttk" not in type_str else 2]
 
     @staticmethod
-    def _get_method_string(component: _ComponentType) -> str:
+    def _get_method_string(component: _ComponentType | Type[T]) -> str:
         return ComponentManager._pascal_to_snake(
             pascal_string = ComponentManager._get_component_type(
                 component = component
             )
         )
+
+    @staticmethod
+    def _check_component_name(input_component_name: str, component: _ComponentType) -> str:
+        component_class_string = ComponentManager._pascal_to_snake(
+            pascal_string = ComponentManager._get_component_type(
+                component = component
+            )
+        )
+
+        if input_component_name.endswith(component_class_string):
+            return input_component_name
+
+        else:
+            return input_component_name + "_" + component_class_string
 
     def add_component(self,
                       name: str,
@@ -42,23 +56,38 @@ class ComponentManager:
                       render_param: Optional[RenderParamWrapper] = None
                       ):
         # component 파라미터의 타입에 알맞은 상속받은 ComponentContainer 클래스의 add메소드
-        getattr(self._component_container, f"add_{self._get_method_string(component)}")(name, component)
+        getattr(self._component_container, f"add_{self._get_method_string(component)}")(
+            ComponentManager._check_component_name(name, component), component
+        )
 
         if with_render:
             getattr(component, render_type)(**render_param.get_param_dict())
 
     def get_component(self, name: str, component_class: Type[T]) -> T:
         # component_class 파라미터의 타입에 알맞은 상속받은 ComponentContainer 클래스의 get메소드
-        return getattr(self._component_container, f"get_{self._get_method_string(component_class)}")(name)
+        return getattr(self._component_container, f"get_{self._get_method_string(component_class)}")(
+            ComponentManager._check_component_name(name, component_class)
+        )
 
     def update_component(self, name: str, component_class: Type[T], **update_params) -> Optional[Exception]:
         component = self.get_component(name = name, component_class = component_class)
 
         try:
+            image_data = None
+            if "image" in update_params.keys():
+                image_data = update_params.pop("image")
+
             component.config(**update_params)
+            if image_data is not None:
+                component.configure(image = image_data)
+                component.image = image_data
+
             component.update()
 
         except Exception as E:
             return E
 
+    @property
+    def component_container(self) -> ComponentContainer:
+        return self._component_container
 
